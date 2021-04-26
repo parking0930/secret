40a2e2d8d83b271ca3a1e702f43da13fc692e482
parking0930 <parking0930@naver.com>
2021-04-26 21:08:59 +0900

    feat: design

--- app/src/main/java/com/inhatc/secret/LoginActivity.java
+++ app/src/main/java/com/inhatc/secret/LoginActivity.java
@@ -2,13 +2,27 @@ package com.inhatc.secret;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent MainIntent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(MainIntent);
+            }
+        });
     }
 }
